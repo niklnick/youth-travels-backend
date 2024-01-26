63319cbc61a83fe7c1b2b89e76b97288a1634967
neumann.niklas <neumann.niklas98@gmail.com>
2024-01-26 16:00:09 +0100

    docker, config & typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm-config.service';
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })
+    ]
+})
 export class AppModule { }
---
+++ src/config/typeorm-config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private readonly configService: ConfigService) { }
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.configService.get<string>('POSTGRES_HOST'),
+            port: this.configService.get<number>('POSTGRES_PORT'),
+            username: this.configService.get<string>('POSTGRES_USERNAME'),
+            password: this.configService.get<string>('POSTGRES_PASSWORD'),
+            database: this.configService.get<string>('POSTGRES_DATABASE'),
+            autoLoadEntities: true,
+            synchronize: true
+        };
+    }
+}
